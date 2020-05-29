2be67261183f046492ce38c109cc2b5d59f03712
hejohns <hejohns@umich.edu>
2020-05-28 23:06:23 -0400

    can't use xor for address caluclation

--- mymalloc.c
+++ mymalloc.c
@@ -6,7 +6,7 @@
 /* A very simple buddy? allocator
  *
  * Partially to reduce extreme fragmentation--
- * mostly just cause this is an educational project
+ * but mostly just cause this is an educational project
  */
 
 // if I'm correct, __bss_end is a dummy compiler defined global variable 
@@ -16,7 +16,8 @@
 // 1) move the bss section as well, or 2) change some stuff around here
 // (The prior seems most logical)
 extern char __bss_end;
-static volatile char* mymalloc_start = (&__bss_end)+1;
+static volatile char* myheap_start = (&__bss_end)+1;
+static volatile char* myheap_end = mymalloc_start;
 // __heap_start/__heap_end are also compiler defined... but I couldn't find it
 // in the elf dump for this commit
 // (granted I'm not using avr libc's malloc headers)
