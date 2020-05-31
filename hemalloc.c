8d5a09bc79795a6c5aa061c64ac7e8d600e19b65
Johnson He <hejohns@umich.edu>
2020-05-30 23:28:31 -0400

    There's a decision to be made here for malloc:
    I was going to use a buddy system, but depending on the initial size,
    that would leave headers for the later fragments way past actually used memory,
    which might collide with the stack needlessly (ex- given a 64kb heap, allocating just
    1 byte would create a header at 32kb). A simple first fit system would solve this,
    but of course since I do actually intend to use malloc so a small degree for the shell
    (maybe this is a stupid idea), I would prefer not to have the tiny fragments associated with
    such a malloc scheme. I was thinking about just using a compile time number of chunks of
    some compile time decided size. But that's also so coarse it's just gross. I think I may end
    up just using a first fit. But since the decision is not entirely clear, I will go back to
    doing 281 p3 until I've made up my mind. Also some crazy part of me wants to port this to 8086
    using real mode (I mean... what else am I going to do with those pentium n3700 nucs) just for fun.
    As far as I'm aware, given a suitable dos compiler, very little has to be rewritten (ofc the avr specific timer
    stuff and register dumps). (I pray what I have so far minus the gcc attributes is C90 without too much work)
    
    To my future self: after implementing "kernel level" malloc, and some basic shell functionality, I really
    do want to start getting into the whole mmu schpiel. That's why I wanted to start with x86 real mode and
    move into working with protected mode. Hopefully someday I'll get there.
    Also after a sh for avr, yknow it'd be cool to like actually do something that uses simos. Maybe like a
    little light show or something idk not really a hardware guy. Just something to flash and shelf for a bit.
    And bust it out every now and then.

--- hemalloc.c
+++ hemalloc.c
@@ -4,28 +4,27 @@
 #include <string.h>
 #include "hemalloc.h"
 
-/* A very simple buddy? allocator
+/* A very simple .* allocator
  *
- * Partially to reduce extreme fragmentation--
- * but mostly just cause this is an educational project
  */
 
-// if I'm correct, __bss_end is a dummy compiler defined global variable 
-// to serve as the address of the end of the .bss section. 
-// We will start heap directly after &__bss_end. 
-// If you wanted to move the heap to external ram, either 
-// 1) move the bss section as well, or 2) change some stuff around here
-// (The prior seems most logical)
+/* if I'm correct, __bss_end is a dummy compiler defined global variable 
+ * to serve as the address of the end of the .bss section. 
+ * We will start heap directly after &__bss_end. 
+ * If you wanted to move the heap to external ram, either 
+ * 1) move the bss section as well, or 2) change some stuff around here
+ * (The prior seems most logical)
+ */
 extern char __bss_end;
 static volatile char* myheap_start = (&__bss_end)+1;
 static volatile char* myheap_end = hemalloc_start;
-// __heap_start/__heap_end are also compiler defined... but I couldn't find it
-// in the elf dump for this commit
-// (granted I'm not using avr libc's malloc headers)
+/* __heap_start/__heap_end are also compiler defined... but I couldn't find it
+ * in the elf dump for this commit
+ * (granted I'm not using avr libc's malloc headers)
+ */
 
 static struct header{
     size_t size; // size includes header; thus size is always a power of 2
-    struct header* next;
 };
 static struct header free_list_head = {.size=0x8000, .next=NULL}; // size_t is 2 bytes
 
